7b72be6b83dd725a9e35965852b326c12600455a
hoooon-97 <53173232+hoooon-97@users.noreply.github.com>
2022-05-08 20:56:04 +0900

    update

---
+++ detectme/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views.decorators import gzip
+from django.http import StreamingHttpResponse
+from detectme.camera import FaceDetect
+
+def home(request):
+    return render(request, 'home.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield(b'--frame\r\n'
+              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@gzip.gzip_page
+def detectme(request):
+    return StreamingHttpResponse(gen(FaceDetect()), content_type="multipart/x-mixed-replace;boundary=frame")
