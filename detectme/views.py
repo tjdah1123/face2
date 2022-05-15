285b6c83406aacce2bb828cf95e9a6aae86cfac4
hoooon-97 <53173232+hoooon-97@users.noreply.github.com>
2022-05-15 20:58:09 +0900

    update 5.15

--- detectme/views.py
+++ detectme/views.py
@@ -12,7 +12,9 @@ def gen(camera):
         yield(b'--frame\r\n'
               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-
 @gzip.gzip_page
 def detectme(request):
     return StreamingHttpResponse(gen(FaceDetect()), content_type="multipart/x-mixed-replace;boundary=frame")
+
+def check(request):
+    return render(request, "check.html")
